ddbf9e584f076526b57136510cfb02d42fe99a23
Sayyaf <athmanbilal16@gmail.com>
2026-08-05 18:22:36 +0300

    Update dashboard.js

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -1,1 +1,18 @@
+const ring = document.getElementById("ringProgress");
+const percentText = document.getElementById("progressPercent");
 
+function setPrayerProgress(percent){
+
+const circumference = 597;
+
+const offset = circumference - (percent / 100) * circumference;
+
+ring.style.strokeDashoffset = offset;
+
+percentText.textContent = Math.round(percent) + "%";
+
+}
+
+/* Demo */
+
+setPrayerProgress(35);
